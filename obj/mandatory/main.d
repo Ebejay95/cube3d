5185556c0cda4e6b3da9d743a35f1f4c9acf64ed
Jonathan Eberle <jeberle@student.42heilbronn.de>
2024-09-17 15:16:04 +0200

    initial commit

---
+++ obj/mandatory/main.d
@@ -0,0 +1,9 @@
+obj/mandatory/main.o: mandatory/main.c mandatory/./../includes/cube3d.h \
+  mandatory/./../includes/./../libft/libft.h \
+  mandatory/./../libft/libft.h
+
+mandatory/./../includes/cube3d.h:
+
+mandatory/./../includes/./../libft/libft.h:
+
+mandatory/./../libft/libft.h:
